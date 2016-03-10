c24e27afb748bd66f3b4551224989efc11bec9fd
Luis Pabón <lpabon@redhat.com>
2016-03-09 21:08:58 -0500

    delay in setup_gluster

--- roles/kub_master/files/setup_gluster.sh
+++ roles/kub_master/files/setup_gluster.sh
@@ -2,6 +2,8 @@
 kubectl exec gluster-1 -- gluster volume info
 kubectl exec gluster-1 -- gluster peer probe 192.168.10.101
 kubectl exec gluster-1 -- gluster peer probe 192.168.10.102
+sleep 5
 kubectl exec gluster-1 -- gluster volume create glustervol replica 3 192.168.10.100:/srv/brick/brick1 192.168.10.101:/srv/brick/brick1 192.168.10.102:/srv/brick/brick1
+sleep 1
 kubectl exec gluster-1 -- gluster volume start glustervol
 
